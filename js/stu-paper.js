c9eb04b43154408134a6a09cb84901897d93c488
chaeryeongsdog <xian920626@gmail.com>
2024-05-09 02:37:32 +0800

    試卷分頁ok

--- js/stu-paper.js
+++ js/stu-paper.js
@@ -1,4 +1,98 @@
+var chinese = document.getElementById("chinese");
+var math = document.getElementById("math");
+var english = document.getElementById("english");
+var natural = document.getElementById("natural");
+var society = document.getElementById("society");
 
+chinese.addEventListener("click",function(){
+    document.getElementById("tab-chinese").style.display="flex";
+    document.getElementById("tab-math").style.display="none";
+    document.getElementById("tab-english").style.display="none";
+    document.getElementById("tab-natural").style.display="none";
+    document.getElementById("tab-society").style.display="none";
+    chinese.style.backgroundColor="#034A8C";
+    chinese.style.color="white";
+    math.style.backgroundColor="#d9d9d9";
+    math.style.color="black"
+    english.style.backgroundColor="#d9d9d9";
+    english.style.color="black"
+    natural.style.backgroundColor="#d9d9d9";
+    natural.style.color="black"
+    society.style.backgroundColor="#d9d9d9";
+    society.style.color="black"
+});
+
+math.addEventListener("click",function(){
+    document.getElementById("tab-chinese").style.display="none";
+    document.getElementById("tab-math").style.display="flex";
+    document.getElementById("tab-english").style.display="none";
+    document.getElementById("tab-natural").style.display="none";
+    document.getElementById("tab-society").style.display="none";
+    chinese.style.backgroundColor="#d9d9d9";
+    chinese.style.color="black"
+    math.style.backgroundColor="#034A8C";
+    math.style.color="white";
+    english.style.backgroundColor="#d9d9d9";
+    english.style.color="black"
+    natural.style.backgroundColor="#d9d9d9";
+    natural.style.color="black"
+    society.style.backgroundColor="#d9d9d9";
+    society.style.color="black"
+});
+
+english.addEventListener("click",function(){
+    document.getElementById("tab-chinese").style.display="none";
+    document.getElementById("tab-math").style.display="none";
+    document.getElementById("tab-english").style.display="flex";
+    document.getElementById("tab-natural").style.display="none";
+    document.getElementById("tab-society").style.display="none";
+    chinese.style.backgroundColor="#d9d9d9";
+    chinese.style.color="black"
+    math.style.backgroundColor="#d9d9d9";
+    math.style.color="black"
+    english.style.backgroundColor="#034A8C";
+    english.style.color="white";
+    natural.style.backgroundColor="#d9d9d9";
+    natural.style.color="black"
+    society.style.backgroundColor="#d9d9d9";
+    society.style.color="black"
+});
+
+natural.addEventListener("click",function(){
+    document.getElementById("tab-chinese").style.display="none";
+    document.getElementById("tab-math").style.display="none";
+    document.getElementById("tab-english").style.display="none";
+    document.getElementById("tab-natural").style.display="flex";
+    document.getElementById("tab-society").style.display="none";
+    chinese.style.backgroundColor="#d9d9d9";
+    chinese.style.color="black"
+    math.style.backgroundColor="#d9d9d9";
+    math.style.color="black"
+    english.style.backgroundColor="#d9d9d9";
+    english.style.color="black";
+    natural.style.backgroundColor="#034A8C";
+    natural.style.color="white";
+    society.style.backgroundColor="#d9d9d9";
+    society.style.color="black"
+});
+
+society.addEventListener("click",function(){
+    document.getElementById("tab-chinese").style.display="none";
+    document.getElementById("tab-math").style.display="none";
+    document.getElementById("tab-english").style.display="none";
+    document.getElementById("tab-natural").style.display="none";
+    document.getElementById("tab-society").style.display="flex";
+    chinese.style.backgroundColor="#d9d9d9";
+    chinese.style.color="black"
+    math.style.backgroundColor="#d9d9d9";
+    math.style.color="black"
+    english.style.backgroundColor="#d9d9d9";
+    english.style.color="black";
+    natural.style.backgroundColor="#d9d9d9";
+    natural.style.color="black";
+    society.style.backgroundColor="#034A8C";
+    society.style.color="white";
+});
 document.addEventListener('DOMContentLoaded',function(){
     var token = localStorage.getItem('JwtToken');
     var userInfo = localStorage.getItem('userInfo');
