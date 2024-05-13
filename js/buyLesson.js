5fb650dee50ebb5a95503992f92c75d91601f569
chaeryeongsdog <xian920626@gmail.com>
2024-05-13 18:47:18 +0800

    結帳方式

--- js/buyLesson.js
+++ js/buyLesson.js
@@ -553,3 +553,13 @@ function logout(){
         console.error('登出操作中发生错误:', error);
     });
     }
+
+const dialog = document.querySelector('dialog');
+function openDialog() {
+  dialog.showModal(dialog);
+  dialog.style.display="flex";
+}
+function closeDialog() {
+  dialog.close();
+  dialog.style.display="none"
+}
